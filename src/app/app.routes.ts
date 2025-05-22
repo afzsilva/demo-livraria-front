42be2891257c5b0e5ca146dd2de3b87177d9aa7f
afzsilva <afzsilva@gmail.com>
2025-05-21 23:33:08 -0300

    Atualiza rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LivrosComponent } from './page/livros/livros.component';
+import { AutoresComponent } from './page/autores/autores.component';
+import { GenerosComponent } from './page/generos/generos.component';
 
 export const routes: Routes = [
     {
         path: 'livros',
         component: LivrosComponent,
+    },
+    {
+        path: 'autores',
+        component: AutoresComponent,
+    },
+    {
+        path: 'generos',
+        component: GenerosComponent,
     }
 ];
