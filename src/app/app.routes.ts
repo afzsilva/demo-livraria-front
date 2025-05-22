5311c677249f297353c04f7a8695754130715026
afzsilva <afzsilva@gmail.com>
2025-05-21 23:19:15 -0300

    Cria primeira rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LivrosComponent } from './page/livros/livros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'livros',
+        component: LivrosComponent,
+    }
+];
