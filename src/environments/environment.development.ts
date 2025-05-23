6d3865cd6f99376c4a8ccfdce4b34ceea2367957
afzsilva <afzsilva@gmail.com>
2025-05-22 23:31:12 -0300

    Ajustes enviroment para build gerar dist

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
-export const environment = {    
+export const environment = {
     urls: {
         autores: 'http://localhost:8080/api/autores',
         generos: 'http://localhost:8080/api/generos',
         livros: 'http://localhost:8080/api/livros',      
-      },    
+      },
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,8 @@
-export const environment = {    
+export const environment = {
+  production:true,
     urls: {
         autores: 'http://localhost:8080/api/autores',
         generos: 'http://localhost:8080/api/generos',
         livros: 'http://localhost:8080/api/livros',      
-      },    
+      },
 };
